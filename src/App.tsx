e6eac606bf79d48341ce009a81cd5209dcc59b0b
leonardodev <leonardoayala03072000@gmail.com>
2024-04-12 14:22:42 -0500

    se agrego paginacion a la tabla

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,63 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Content, ResponseData } from './response'
-
+import { Content } from './response'
+import ReactPaginate from 'react-paginate';
 
 function App() {
-  const [dataFutbolistas,setDataFutbolistas]=useState<Content[]>([]);
+  const [dataFutbolistas, setDataFutbolistas] = useState<Content[]>([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const itemsPerPage = 5;
+  const [pageCount, setPageCount] = useState(0);
 
-  const getDataFutbolista=async()=>{
-    const response=await fetch("http://localhost:8080/api/futbolista/page",{
-      method:"GET"
-    })
-    const jsonData:ResponseData= await response.json();
-    console.log(jsonData);
-    setDataFutbolistas(jsonData.content);
+  const getDataFutbolista = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/futbolista/page?page=${pageNumber}&size=${itemsPerPage}`, {
+        method: "GET"
+      });
+      const jsonData = await response.json();
+      setDataFutbolistas(jsonData.content);
+      setPageCount(jsonData.totalPages);
+    } catch (error) {
+      console.error("Error", error);
+    }
   }
 
-  const verDetalleFutbolista = async(id:number) => {
+  useEffect(() => {
+    getDataFutbolista();
+  }, [pageNumber]); 
+
+  const verDetalleFutbolista = async (id: number) => {
     try {
       const response = await fetch(`http://localhost:8080/api/futbolista/${id}`, {
         method: "GET"
       });
       const futbolistaDetalle = await response.json();
-      console.log(futbolistaDetalle)
-      alert(`Detalles del futbolista : ${futbolistaDetalle.nombres+" "+futbolistaDetalle.apellidos}\nFecha de Nacimiento: ${futbolistaDetalle.fechaNacido}\nPosición: ${futbolistaDetalle.posicion.nombre}\nCaracterísticas: ${futbolistaDetalle.caracteristicas}`);
+      alert(`Detalles del futbolista : ${futbolistaDetalle.nombres} ${futbolistaDetalle.apellidos}\nFecha de Nacimiento: ${futbolistaDetalle.fechaNacido}\nPosición: ${futbolistaDetalle.posicion.nombre}\nCaracterísticas: ${futbolistaDetalle.caracteristicas}`);
     } catch (error) {
-      console.error("Error al obtener detalles", error);
+      console.error("Error", error);
     }
-
   };
 
-  useEffect(()=>{
-    getDataFutbolista();
-  },[])
+  const handlePageClick = ({ selected }: { selected: number }) => {
+    setPageNumber(selected);
+  };
+
+  const data = dataFutbolistas.map(futbolista => (
+    <tr key={futbolista.idFutbolista}>
+      <td>{futbolista.idFutbolista}</td>
+      <td>{futbolista.nombres}</td>
+      <td>{futbolista.apellidos}</td>
+      <td>{futbolista.posicion.nombre}</td>
+      <td>
+        <button onClick={() => verDetalleFutbolista(futbolista.idFutbolista)}>Ver Detalle</button>
+      </td>
+    </tr>
+  ));
 
   return (
-<div className='container'>
-    <h1 className="title">Lista de Futbolistas</h1>
-    <table  className="table">
+    <div className='container'>
+      <h1 className="title">Lista de Futbolistas</h1>
+      <table className="table">
         <thead>
           <tr>
             <th>ID</th>
@@ -47,21 +68,19 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {dataFutbolistas.map((futbolista) => (
-            <tr key={futbolista.idFutbolista}>
-              <td>{futbolista.idFutbolista}</td>
-              <td>{futbolista.nombres}</td>
-              <td>{futbolista.apellidos}</td>
-              <td>{futbolista.posicion.nombre}</td>
-              <td>
-                <button  onClick={() => verDetalleFutbolista(futbolista.idFutbolista)}>Ver Detalle</button>
-              </td>
-            </tr>
-          ))}
+          {data}
         </tbody>
       </table>
-      </div>
-  )
+      <ReactPaginate
+        previousLabel={'Anterior'}
+        nextLabel={'Siguiente'}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
